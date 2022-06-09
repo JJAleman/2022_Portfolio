585b89f1c6fc645610a46cd9835e91c0c9bd191b
Juan Aleman <juan.j.aleman@gmail.com>
2022-06-09 01:32:01 -0500

    Added background color and able to switch between sections

--- app.js
+++ app.js
@@ -12,5 +12,24 @@ function PageTransition() {
             this.className += ' active-btn';
         })
     }
+    //Sections active class
+    allSections[0].addEventListener('click', (e) => {
+            const id = e.target.dataset.id; 
+            if(id){
+                //remove selected from the other bttns
+                sectBtns.forEach((btn) => {
+                    btn.classList.remove('active')
+                })
+                e.target.classList.add('active')
+
+                //hide other sections
+                sections.forEach((section) => {
+                    section.classList.remove('active')
+                })
+
+                const element = document.getElementById(id);
+                element.classList.add('active')
+            }
+        })
 }
 PageTransition();
